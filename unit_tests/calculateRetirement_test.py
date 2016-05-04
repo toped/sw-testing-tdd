25d2a90b7fef5b7c87bc0786c80ced0594aac6a8
dtj74 <dylan.jaye@gmail.com>
2016-05-04 15:53:44 -0500

    3 test additions
    Added 3 tests which ensure functionality of calculateRetirement function

--- unit_tests/calculateRetirement_test.py
+++ unit_tests/calculateRetirement_test.py
@@ -3,5 +3,14 @@ import pytest
 from main import calculateRetirement
 
 
-def test_example_1():
-    assert calculateRetirement(20, 50000, 20, 100000) == 25
+# test for expected basic functionality
+def test_expected_outcome():
+    assert calculateRetirement(20, 50000, 20, 100000) == 30
+
+# test that a retirement age >=100 will notify the user
+def test_goal_not_met():
+    assert calculateRetirement(40, 20000, 20, 500000) == "You will not meet your savings goal"
+
+# test if an age is entered which already exceeds max age
+def test_age():
+    assert calculateRetirement(100, 20000, 20, 500000) == "The age you entered is too high.  You shouldn't be working!"
