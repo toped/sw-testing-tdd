e76fe76d56e75c0b1ea8f23bb5650df2bbea98be
Dylan <dylan.jaye@gmail.com>
2016-05-04 15:54:14 -0500

    Adding retirement calculator changes to Master

--- unit_tests/calculateRetirement_test.py
+++ unit_tests/calculateRetirement_test.py
@@ -3,5 +3,13 @@ import pytest
 from main import calculateRetirement
 
 
-def test_example_1():
-    assert calculateRetirement(20, 50000, 20, 100000) == 25
+# test for expected basic functionality
+def test_expected_outcome():
+    assert calculateRetirement(20, 50000, 20, 100000) == 30
+
+# test that a retirement age >=100 will notify the user
+def test_goal_not_met():
+    assert calculateRetirement(40, 20000, 20, 500000) == "You will not meet your savings goal"
+
+def test_age():
+    assert calculateRetirement(100, 20000, 20, 500000) == "The age you entered is too high.  You shouldn't be working!"
